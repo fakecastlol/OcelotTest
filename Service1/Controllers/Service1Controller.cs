fdf0b6d0fc661f90d1e91746bc25fbaa495c8bf0
fakecastlol <49000213+fakecastlol@users.noreply.github.com>
2020-11-11 14:16:29 +0300

    Initial commit

---
+++ Service1/Controllers/Service1Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Service1.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class Service1Controller : ControllerBase
+    {
+        [HttpGet("service1")]
+        public string Get()
+        {
+            return "This is the 1st service.";
+        }
+    }
+}
---
+++ Service2/Controllers/Service2Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Service2.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class Service2Controller : ControllerBase
+    {
+        [HttpGet("service2")]
+        public string Get()
+        {
+            return "This is the 2nd service.";
+        }
+    }
+}
